a90e461e827f5392dd247e3ee0937c01607baa69
S'phesihle Ndlovu <48141384+sphehcarez@users.noreply.github.com>
2022-11-17 14:56:39 +0200

    Update theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -9,3 +9,12 @@ export const roadmapTheme = extendTheme({
     }
   }
 });
+export const roadmapTheme = extendTheme({
+  colors: {
+    brand: {
+      bg: '#06020d',
+      hero: '#06020d',
+      footer: '#0d041e'
+    }
+  }
+});
